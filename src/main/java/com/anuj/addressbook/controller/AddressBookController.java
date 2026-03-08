d0f981066214a770077b853f95dc41d9a1d8a296
Anuj940 <anujvishwakarma9827@gmail.com>
2026-03-09 00:28:37 +0530

    [Anuj] : added AddressBookController

---
+++ src/main/java/com/anuj/addressbook/controller/AddressBookController.java
@@ -0,0 +1,24 @@
+package com.anuj.addressbook.controller;
+
+import com.anuj.addressbook.model.Contact;
+import com.anuj.addressbook.service.AddressBookService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/addressbooks")
+public class AddressBookController {
+
+    private final AddressBookService service;
+
+    public AddressBookController(AddressBookService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/{name}/contacts")
+    public Contact addContact(
+            @PathVariable String name,
+            @RequestBody Contact contact) {
+
+        return service.addContact(name, contact);
+    }
+}
