cdaa0b4658f77b28aa13a4166abd3ead37c9c833
Anuj940 <anujvishwakarma9827@gmail.com>
2026-03-09 00:56:24 +0530

    [Anuj] : added AddressBookService

--- src/main/java/com/anuj/addressbook/service/AddressBookService.java
+++ src/main/java/com/anuj/addressbook/service/AddressBookService.java
@@ -4,6 +4,7 @@ import com.anuj.addressbook.model.AddressBook;
 import com.anuj.addressbook.model.Contact;
 import org.springframework.stereotype.Service;
 
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -24,6 +25,35 @@ public class AddressBookService {
         book.addContact(contact);
 
         return contact;
+    }
+
+    public Contact updateContact(String bookName, String firstName, String lastName, Contact updated) {
+
+        AddressBook book = addressBooks.get(bookName);
+
+        if (book == null) {
+            return null;
+        }
+
+        for (Contact contact : book.getContacts()) {
+
+            if (contact.getFirstName().equals(firstName) &&
+                contact.getLastName().equals(lastName)) {
+
+                contact.setFirstName(updated.getFirstName());
+                contact.setLastName(updated.getLastName());
+                contact.setAddress(updated.getAddress());
+                contact.setCity(updated.getCity());
+                contact.setState(updated.getState());
+                contact.setZip(updated.getZip());
+                contact.setPhoneNumber(updated.getPhoneNumber());
+                contact.setEmail(updated.getEmail());
+
+                return contact;
+            }
+        }
+
+        return null;
     }
 
     public AddressBook getAddressBook(String name) {
