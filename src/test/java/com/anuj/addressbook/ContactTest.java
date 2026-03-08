6e45e708864a3e8055f72cc2b27cd087d3c04cb1
Anuj940 <anujvishwakarma9827@gmail.com>
2026-03-09 00:12:49 +0530

    [Anuj] : added ContactTest

--- src/test/java/com/anuj/addressbook/ContactTest.java
+++ src/test/java/com/anuj/addressbook/ContactTest.java
@@ -1,6 +1,6 @@
 package com.anuj.addressbook;
 
-import com.anuj.addressbook.Contact;
+import com.anuj.addressbook.model.Contact;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
