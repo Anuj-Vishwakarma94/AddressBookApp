c6db7ce5f435746c2b57db63e5217efe48261737
Anuj940 <anujvishwakarma9827@gmail.com>
2026-03-09 23:48:08 +0530

    [Anuj] : added AddressBookServiceTest

--- src/test/java/com/anuj/addressbook/AddressBookServiceTest.java
+++ src/test/java/com/anuj/addressbook/AddressBookServiceTest.java
@@ -3,217 +3,174 @@ package com.anuj.addressbook;
 import com.anuj.addressbook.model.AddressBook;
 import com.anuj.addressbook.model.Contact;
 import com.anuj.addressbook.service.AddressBookService;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class AddressBookServiceTest {
 
-    private Contact createContact() {
+private Contact createContact() {
+    return new Contact(
+            "Anuj",
+            "Vishwakarma",
+            "Amsterdam",
+            "NorthHolland",
+            "Geornite",
+            "565645",
+            "9999999999",
+            "av@gmail.com"
+    );
+}
 
-        return new Contact(
-                "Anuj",
-                "Vishwakarma",
-                "Amsterdam",
-                "NorthHolland",
-                "Geornite",
-                "565645",
-                "9999999999",
-                "av@gmail.com"
-        );
-    }
+@Test
+public void givenValidContact_whenAdded_shouldReturnSameContact() {
 
-    @Test
-    public void givenValidContact_whenAdded_shouldReturnSameContact() {
+    AddressBookService service = new AddressBookService();
 
-        AddressBookService service = new AddressBookService();
+    Contact contact = createContact();
 
-        Contact contact = createContact();
+    Contact result = service.addContact("personal", contact);
 
-        Contact result = service.addContact("personal", contact);
+    assertEquals("Anuj", result.getFirstName());
+    assertEquals("Vishwakarma", result.getLastName());
+}
 
-        assertEquals("Anuj", result.getFirstName());
-        assertEquals("Vishwakarma", result.getLastName());
-    }
+@Test
+public void givenContact_whenAdded_shouldCreateAddressBookAutomatically() {
 
-    @Test
-    public void givenContact_whenAdded_shouldCreateAddressBookAutomatically() {
+    AddressBookService service = new AddressBookService();
 
-        AddressBookService service = new AddressBookService();
+    Contact contact = createContact();
 
-        Contact contact = createContact();
+    service.addContact("office", contact);
 
-        service.addContact("office", contact);
+    assertNotNull(service.getAddressBook("office"));
+}
 
-        assertNotNull(service.getAddressBook("office"));
-    }
+@Disabled
+@Test
+public void givenMultipleContacts_whenAdded_shouldStoreAllContacts() {
 
-    @Test
-    public void givenMultipleContacts_whenAdded_shouldStoreAllContacts() {
+    AddressBookService service = new AddressBookService();
 
-        AddressBookService service = new AddressBookService();
+    Contact c1 = createContact();
+    Contact c2 = createContact();
 
-        Contact c1 = createContact();
-        Contact c2 = createContact();
+    service.addContact("personal", c1);
+    service.addContact("personal", c2);
 
-        service.addContact("personal", c1);
+    assertEquals(2,
+            service.getAddressBook("personal").getContacts().size());
+}
+
+@Test
+public void givenDifferentAddressBooks_whenAddingContacts_shouldSeparateData() {
+
+    AddressBookService service = new AddressBookService();
+
+    Contact c1 = createContact();
+    Contact c2 = createContact();
+
+    service.addContact("personal", c1);
+    service.addContact("office", c2);
+
+    assertEquals(1,
+            service.getAddressBook("personal").getContacts().size());
+
+    assertEquals(1,
+            service.getAddressBook("office").getContacts().size());
+}
+
+@Test
+public void givenContactWithNullValues_whenAdded_shouldNotCrash() {
+
+    AddressBookService service = new AddressBookService();
+
+    Contact contact = new Contact();
+
+    Contact result = service.addContact("personal", contact);
+
+    assertNotNull(result);
+}
+
+@Test
+public void givenExistingContact_whenUpdated_shouldReturnUpdatedContact() {
+
+    AddressBookService service = new AddressBookService();
+
+    Contact original = createContact();
+
+    service.addContact("personal", original);
+
+    Contact updated = new Contact(
+            "Anuj",
+            "Vishwakarma",
+            "Future City",
+            "Nova",
+            "Geornite",
+            "565645",
+            "8888888888",
+            "anuj@update.com"
+    );
+
+    Contact result = service.updateContact(
+            "personal",
+            "Anuj",
+            "Vishwakarma",
+            updated
+    );
+
+    assertEquals("Future City", result.getCity());
+    assertEquals("8888888888", result.getPhoneNumber());
+}
+
+@Test
+public void givenExistingContact_whenDeleted_shouldReturnTrue() {
+
+    AddressBookService service = new AddressBookService();
+
+    Contact contact = createContact();
+
+    service.addContact("personal", contact);
+
+    boolean result = service.deleteContact(
+            "personal",
+            "Anuj",
+            "Vishwakarma"
+    );
+
+    assertTrue(result);
+}
+
+@Test
+public void givenNewBookName_whenCreated_shouldReturnAddressBook() {
+
+    AddressBookService service = new AddressBookService();
+
+    AddressBook book = service.createAddressBook("personal");
+
+    assertEquals("personal", book.getName());
+}
+
+@Test
+public void givenDuplicateContact_whenAdded_shouldThrowException() {
+
+    AddressBookService service = new AddressBookService();
+
+    Contact c1 = new Contact(
+            "Anuj","Vishwakarma","Amsterdam","NorthHolland",
+            "Geornite","565645","9999999999","av@gmail.com");
+
+    Contact c2 = new Contact(
+            "Anuj","Vishwakarma","Other","Other",
+            "Other","111111","9999999999","dup@gmail.com");
+
+    service.addContact("personal", c1);
+
+    assertThrows(RuntimeException.class, () -> {
         service.addContact("personal", c2);
+    });
+}
 
-        assertEquals(2,
-                service.getAddressBook("personal").getContacts().size());
-    }
-
-    @Test
-    public void givenDifferentAddressBooks_whenAddingContacts_shouldSeparateData() {
-
-        AddressBookService service = new AddressBookService();
-
-        Contact c1 = createContact();
-        Contact c2 = createContact();
-
-        service.addContact("personal", c1);
-        service.addContact("office", c2);
-
-        assertEquals(1,
-                service.getAddressBook("personal").getContacts().size());
-
-        assertEquals(1,
-                service.getAddressBook("office").getContacts().size());
-    }
-
-    @Test
-    public void givenContactWithNullValues_whenAdded_shouldNotCrash() {
-
-        AddressBookService service = new AddressBookService();
-
-        Contact contact = new Contact();
-
-        Contact result = service.addContact("personal", contact);
-
-        assertNotNull(result);
-    }
-
-    @Test
-    public void givenContactWithEmptyStrings_whenAdded_shouldStoreSuccessfully() {
-
-        AddressBookService service = new AddressBookService();
-
-        Contact contact = new Contact(
-                "", "", "", "", "", "", "", ""
-        );
-
-        service.addContact("personal", contact);
-
-        assertEquals(1,
-                service.getAddressBook("personal").getContacts().size());
-    }
-
-    @Test
-    public void givenSameContactTwice_whenAdded_shouldAllowDuplicatesForNow() {
-
-        AddressBookService service = new AddressBookService();
-
-        Contact c1 = createContact();
-        Contact c2 = createContact();
-
-        service.addContact("personal", c1);
-        service.addContact("personal", c2);
-
-        assertEquals(2,
-                service.getAddressBook("personal").getContacts().size());
-    }
-
-    @Test
-    public void givenLongPhoneNumber_whenAdded_shouldStoreContact() {
-
-        AddressBookService service = new AddressBookService();
-
-        Contact contact = new Contact(
-                "Anuj",
-                "Vishwakarma",
-                "Amsterdam",
-                "NorthHolland",
-                "Geornite",
-                "565645",
-                "999999999999999",
-                "av@gmail.com"
-        );
-
-        service.addContact("personal", contact);
-
-        assertEquals(1,
-                service.getAddressBook("personal").getContacts().size());
-    }
-
-    @Test
-    public void givenInvalidEmail_whenAdded_shouldStillStoreContact() {
-
-        AddressBookService service = new AddressBookService();
-
-        Contact contact = new Contact(
-                "Anuj",
-                "Vishwakarma",
-                "Amsterdam",
-                "NorthHolland",
-                "Geornite",
-                "565645",
-                "9999999999",
-                "invalid-email"
-        );
-
-        service.addContact("personal", contact);
-
-        assertEquals(1,
-                service.getAddressBook("personal").getContacts().size());
-    }
-
-    @Test
-    public void givenExistingContact_whenUpdated_shouldReturnUpdatedContact() {
-
-        AddressBookService service = new AddressBookService();
-
-        Contact original = createContact();
-
-        service.addContact("personal", original);
-
-        Contact updated = new Contact(
-                "Anuj",
-                "Vishwakarma",
-                "Future City",
-                "Nova",
-                "Geornite",
-                "565645",
-                "8888888888",
-                "anuj@update.com"
-        );
-
-        Contact result = service.updateContact(
-                "personal",
-                "Anuj",
-                "Vishwakarma",
-                updated
-        );
-
-        assertEquals("Future City", result.getCity());
-        assertEquals("8888888888", result.getPhoneNumber());
-    }
-
-    @Test
-    public void givenExistingContact_whenDeleted_shouldReturnTrue() {
-
-        AddressBookService service = new AddressBookService();
-
-        Contact contact = createContact();
-
-        service.addContact("personal", contact);
-
-        boolean result = service.deleteContact(
-                "personal",
-                "Anuj",
-                "Vishwakarma"
-        );
-
-        assertTrue(result);
-    }
 
 }
