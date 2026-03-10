a464759a8b1d8fce840c0b2862b5eb6fd7269cfc
Anuj940 <anujvishwakarma9827@gmail.com>
2026-03-10 12:45:52 +0530

    [Anuj] : added AddressBookServiceTest

--- src/test/java/com/anuj/addressbook/AddressBookServiceTest.java
+++ src/test/java/com/anuj/addressbook/AddressBookServiceTest.java
@@ -720,6 +720,63 @@ public class AddressBookServiceTest {
                 new Contact("Rahul","Verma","","NewJersey","DL","","",""));
 
         Map<String, Long> result = service.countContactsByCity();
+
         assertEquals(2, result.get("NewJersey"));
+    }
+    
+    @Test
+    public void givenContacts_whenSortedByName_shouldReturnAlphabeticalOrder() {
+
+        AddressBookService service = new AddressBookService();
+
+        service.addContact("personal",
+                new Contact("Anuj","Vishwakarma","","","","","",""));
+
+        service.addContact("personal",
+                new Contact("Amit","Sharma","","","","","",""));
+
+        service.addContact("personal",
+                new Contact("Rahul","Verma","","","","","",""));
+
+        List<Contact> sorted = service.sortContactsByName("personal");
+
+        assertEquals("Amit", sorted.get(0).getFirstName());
+    }
+    
+    @Test
+    public void givenEmptyAddressBook_whenSorted_shouldReturnEmptyList() {
+
+        AddressBookService service = new AddressBookService();
+
+        assertEquals(0, service.sortContactsByName("personal").size());
+    }
+    
+    @Test
+    public void givenSingleContact_whenSorted_shouldReturnSameContact() {
+
+        AddressBookService service = new AddressBookService();
+
+        service.addContact("personal",
+                new Contact("Anuj","Vishwakarma","","","","","",""));
+
+        List<Contact> result = service.sortContactsByName("personal");
+
+        assertEquals(1, result.size());
+    }
+    
+    @Test
+    public void givenAlreadySortedContacts_whenSorted_shouldRemainSame() {
+
+        AddressBookService service = new AddressBookService();
+
+        service.addContact("personal",
+                new Contact("Amit","Sharma","","","","","",""));
+
+        service.addContact("personal",
+                new Contact("Rahul","Verma","","","","","",""));
+
+        List<Contact> sorted = service.sortContactsByName("personal");
+
+        assertEquals("Amit", sorted.get(0).getFirstName());
     }
 }
